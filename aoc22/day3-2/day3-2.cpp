e3b102ccf0392f93ffa22c62db47e27ab1e9eb18
Rhett Winston <rhettius@gmail.com>
2022-12-04 16:26:28 +1100

    more stuff

--- aoc22/day3-2/day3-2.cpp
+++ aoc22/day3-2/day3-2.cpp
@@ -1,20 +1,76 @@
-// day3-2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+#include <vector>
+
+int letterVal(char letter)
+{
+	if (islower(letter))
+	{
+		return letter - 'a' + 1;
+	}
+	else
+	{
+		return letter - 'A' + 27;
+	}
+}
+
+bool hasChar(string s, char c)
+{
+	return find(begin(s), end(s), c) != end(s);
+}
+
+vector<char> letters()
+{
+	vector<char> ret;
+	for (char c = 'a'; c <= 'z'; c++)
+		ret.push_back(c);
+	for (char c = 'A'; c <= 'Z'; c++)
+		ret.push_back(c);
+	return ret;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	vector<string> lines;
+	int sum{ 0 };
+	bool moreLines{ true };
+
+	while (moreLines)
+	{
+		lines.clear();
+		for (int i = 0; i < 3; i++)
+		{
+			string line;
+			if (getline(cin, line))
+			{
+				lines.push_back(line);
+			}
+			else
+			{
+				moreLines = false;
+				break;
+			}
+		}
+
+		for (char c: letters())
+		{
+			bool allHaveChar = lines.size() > 0;
+			for (string s : lines)
+			{
+				allHaveChar &= hasChar(s, c);
+			}
+
+			if (allHaveChar)
+			{
+				sum += letterVal(c);
+				break;
+			}
+		}
+
+	}
+	
+	cout << "sum of badges across groups: " << sum << endl;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
