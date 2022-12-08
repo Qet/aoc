3c7d45e653184f3bdf2fea7c1c265de06f2324dc
Rhett Winston <rhettius@gmail.com>
2022-12-08 19:57:44 +1100

    Day 7 star 2.

--- aoc22/day7-2/day7-2.cpp
+++ aoc22/day7-2/day7-2.cpp
@@ -1,20 +1,136 @@
-// day7-2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <vector>
+#include <memory>
+#include <string>
+#include <map>
+
+using namespace std;
+
+struct Node
+{
+	Node(string name, bool isDir, int size) :
+		Node(name, isDir, size, nullptr)
+	{}
+
+	Node(string name, bool isDir, int size, shared_ptr<Node> parent) :
+		name(name),
+		isDir(isDir),
+		size(size),
+		parent(parent)
+	{}
+
+	bool isDir;
+	string name;
+	int size;
+	map<string, shared_ptr<Node>> list{};
+	shared_ptr<Node> parent;
+
+	void print(int level)
+	{
+		cout << string(level, ' ');
+		cout << "- " << name << " ";
+		if (isDir)
+			cout << "(dir, size=" << size << ")";
+		else
+			cout << "(file, size=" << size << ")";
+		cout << endl;
+		for (auto& item : list)
+		{
+			item.second->print(level + 1);
+		}
+	}
+
+	void calcSize()
+	{
+		if (isDir)
+		{
+			//Calculate size of all children item (including subdirs recursively)
+			for (auto& item : list)
+			{
+				item.second->calcSize();
+			}
+
+			//Add up the sizes once that is done. 
+			for (auto& item : list)
+			{
+				size += item.second->size;
+			}
+		}
+		//If its a file then the size will have already been constructed with the correct value so no more needs to be done. 
+	}
+
+	Node* findSmallestDirLargerThanX(Node* curBestDir, int X)
+	{
+		if (isDir && size >= X && size < curBestDir->size)
+		{
+			curBestDir = this;
+		}
+		for (auto& item : list)
+		{
+			Node* best = item.second->findSmallestDirLargerThanX(curBestDir, X);
+			if (best->size < curBestDir->size)
+			{
+				curBestDir = best;
+			}
+		}
+		return curBestDir;
+	}
+};
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	string line;
+	
+	shared_ptr<Node> root = make_shared<Node>("/", true, 0);
+	root->parent = root;
+	shared_ptr<Node> curDir = root;
+
+	while (getline(cin, line))
+	{
+		if (line.substr(0, 4) == "$ cd")
+		{
+			string changeTo = line.substr(5);
+
+			if (changeTo == "/")
+			{
+				curDir = root;
+			}
+			else if (changeTo == "..")
+			{
+				curDir = curDir->parent;
+			}
+			else
+			{
+				curDir = curDir->list[changeTo];
+			}
+		}
+		else if (line.substr(0, 4) == "$ ls")
+		{
+			while (cin.peek() != (int)('$'))
+			{
+				string file;
+				getline(cin, file);
+				if (cin.eof())
+					break;
+
+				string fileName = file.substr(file.find(' ') + 1);
+				string filePrefix = file.substr(0, file.find(' '));
+				if (filePrefix == "dir")
+				{
+					curDir->list[fileName] = make_shared<Node>(fileName, true, 0, curDir);
+				}
+				else
+				{
+					curDir->list[fileName] = make_shared<Node>(fileName, false, stoi(filePrefix), curDir);
+				}
+			}
+		}
+	}
+
+	root->calcSize();
+	//root->print(0);
+	cout << root->findSmallestDirLargerThanX(root.get(), root->size - 40000000)->size << endl;
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
