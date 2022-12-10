089b254893449e42896a1c2a2adb62a83f2e001a
Rhett Winston <rhettius@gmail.com>
2022-12-10 14:29:07 +1100

    day 9 star 2....

--- aoc22/day9-2/day9-2.cpp
+++ aoc22/day9-2/day9-2.cpp
@@ -1,20 +1,150 @@
-// day9-2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <windows.h>
+
+using namespace std;
+
+struct point
+{
+	point(int x, int y) :
+		x(x), y(y)
+	{}
+
+	point() {}
+
+	int x{ 0 }, y{ 0 };
+
+	bool operator<(const point& other) const
+	{
+		return (this->x + 1000 * this->y) < (other.x + 1000 * other.y);
+	}
+	
+	bool operator==(const point& other)
+	{
+		return this->x == other.x && this->y == other.y;
+	}
+
+	bool operator!=(const point& other)
+	{
+		return !(*this == other);
+	}
+	string coords()
+	{
+		return "(" + to_string(x) + ", " + to_string(y) + ")";
+	}
+
+	void move(char dir)
+	{
+		switch (dir)
+		{
+		case 'R':
+			x++; break;
+		case 'L':
+			x--; break;
+		case 'U':
+			y++; break;
+		case 'D':
+			y--; break;
+		}
+	}
+
+	double dist(point& other)
+	{
+		double dx = this->x - other.x;
+		double dy = this->y - other.y;
+		return sqrt(dx * dx + dy * dy);
+	}
+
+	point relativePoint(int dx, int dy)
+	{
+		point newpoint;
+		newpoint.x = this->x + dx;
+		newpoint.y = this->y + dy;
+		return newpoint;
+	}
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	vector<point> rope(10);
+
+	map<point, int> tailVisited;
+	string line;
+	while (getline(cin, line))
+	{
+		char dir = line.substr(0, 1)[0];
+		int steps = stoi(line.substr(2));
+
+		for (int i = 0; i < steps; i++)
+		{
+			//1: move H
+			rope[0].move(dir);
+			tailVisited[rope[9]]++;
+
+			//move each of the parts of the rope according to the same logic for a short rope move. 
+			for (int j = 0; j < rope.size() - 1; j++)
+			{
+			
+				point& h = rope[j];
+				point& t = rope[j + 1];
+				
+				
+				//2: if distance from T to H is greater than the rope length then move T to the closest square to H that is within 1 square of T. 
+				//rope length is probably sqrt(2) 
+				double dist = h.dist(t);
+				if (dist > 1.5)
+				{
+					vector<point> surroundingPoints;
+					for (int dx = -1; dx <= 1; dx++)
+					{
+						for (int dy = -1; dy <= 1; dy++)
+						{
+							surroundingPoints.push_back(t.relativePoint(dx, dy));
+						}
+					}
+
+					double closestDist = 999; //big enough number...
+					point closestPoint;
+
+					for (point p : surroundingPoints)
+					{
+						if (p.dist(h) < closestDist)
+						{
+							closestDist = p.dist(h);
+							closestPoint = p;
+						}
+					}
+
+					t = closestPoint;
+				}
+			}
+		}
+		//the tail might have moved in the final step. 
+		tailVisited[rope[9]]++;
+	}
+
+	const int visitCount = tailVisited.size();
+
+	//print out the tail (for debugging)
+	//const int gs = 20;
+	//for (int y = gs; y >= -gs; y--)
+	//{
+	//	for (int x = -gs; x <= gs; x++)
+	//	{
+	//		point p(x, y);
+	//		if (p == point(0, 0))
+	//			cout << "s";
+	//		else if (tailVisited[p] > 0)
+	//			cout << "#";
+	//		else
+	//			cout << ".";
+	//	}
+	//	cout << endl;
+	//}
+	cout << endl;
+
+	cout << visitCount << endl;
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
