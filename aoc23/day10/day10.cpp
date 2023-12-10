e7efbf29b81ae2fb71ef6bfc7010dfc0d044d9a5
Rhett Winston <rhettius@gmail.com>
2023-12-10 21:25:33 +1100

    day10 part 2.

--- aoc23/day10/day10.cpp
+++ aoc23/day10/day10.cpp
@@ -8,6 +8,11 @@
 
 using namespace std;
 
+enum class Direction
+{
+	N,S,E,W
+};
+
 struct Coord
 {
 	int r = 0, c = 0;
@@ -28,6 +33,51 @@ struct Coord
 	{
 		return !(*this == other);
 	}
+
+	Direction getDirection(const Coord& toOther) //from me, to other
+	{
+		if (r == toOther.r)
+		{
+			if (c + 1 == toOther.c)
+				return Direction::E;
+			if (c - 1 == toOther.c)
+				return Direction::W;
+		}
+		if (c == toOther.c)
+		{
+			if (r + 1 == toOther.r)
+				return Direction::S;
+			if (r - 1 == toOther.r)
+				return Direction::N;
+		}
+		throw exception("cannot get direction of non-adjacent squares");
+	}
+
+	Coord N() const
+	{
+		return Coord(r - 1, c);
+	}
+	Coord S() const
+	{
+		return Coord(r + 1, c);
+	}
+	Coord E() const
+	{
+		return Coord(r, c + 1);
+	}
+	Coord W() const
+	{
+		return Coord(r, c - 1);
+	}
+
+};
+
+enum class EnclosureStatus
+{
+	UNDEF,
+	RIGHT, 
+	LEFT,
+	PIPE
 };
 
 struct Square
@@ -39,6 +89,8 @@ struct Square
 
 	Coord loc;
 
+	EnclosureStatus encl;
+
 	void setLoc(Coord c)
 	{
 		loc = c;
@@ -47,7 +99,8 @@ struct Square
 	Square():
 		exits{},
 		dist(INT_MAX),
-		isS(false)
+		isS(false),
+		encl(EnclosureStatus::UNDEF)
 	{
 
 	}
@@ -170,8 +223,6 @@ struct Grid
 				assert(foundExit, "failed to find exit");
 				curDist++;
 			}
-
-
 		}
 
 	}
@@ -192,6 +243,26 @@ struct Grid
 		cout << endl;
 	}
 
+	void printEnclosureStatus()
+	{
+		for (const vector<Square>& row : data)
+		{
+			for (const Square& item : row)
+			{
+				if (item.encl == EnclosureStatus::LEFT)
+					cout << 'L';
+				else if (item.encl == EnclosureStatus::RIGHT)
+					cout << 'R';
+				else if (item.encl == EnclosureStatus::PIPE)
+					cout << '#';
+				else
+					cout << 'U';
+			}
+			cout << endl;
+		}
+		cout << endl;
+	}
+
 	int maxDist()
 	{
 		int ret = 0;
@@ -201,6 +272,122 @@ struct Grid
 			{
 				if (item.dist > ret && item.dist != INT_MAX)
 					ret = item.dist;
+			}
+		}
+		return ret;
+	}
+
+	void applyRL(Square* sq, Direction dir)
+	{
+		Coord rightSq = sq->loc;
+		Coord leftSq = sq->loc;
+		switch (dir)
+		{
+		case Direction::N:
+			rightSq.c++;
+			leftSq.c--;
+			break;
+		case Direction::E:
+			rightSq.r++;
+			leftSq.r--;
+			break;
+		case Direction::S:
+			rightSq.c--;
+			leftSq.c++;
+			break;
+		case Direction::W:
+			rightSq.r--;
+			leftSq.r++;
+			break;
+		}
+
+		if (withinGrid(rightSq))
+		{
+			if (data[rightSq.r][rightSq.c].encl != EnclosureStatus::PIPE)
+				data[rightSq.r][rightSq.c].encl = EnclosureStatus::RIGHT;
+		}
+		if (withinGrid(leftSq))
+		{
+			if (data[leftSq.r][leftSq.c].encl != EnclosureStatus::PIPE)
+				data[leftSq.r][leftSq.c].encl = EnclosureStatus::LEFT;
+		}
+
+	}
+
+	void traverseLoopSetEnclosures()
+	{
+		//traverse the loop and set adjacent squares as either right or left of the direction i'm going. 
+
+		Square* curSq = data[locS.r][locS.c].exits[0];
+		Square* prevSq = &data[locS.r][locS.c];
+
+		while (curSq->loc != locS)
+		{
+			Direction dir = prevSq->loc.getDirection(curSq->loc);
+
+			curSq->encl = EnclosureStatus::PIPE;
+
+			applyRL(prevSq, dir);
+			applyRL(curSq, dir);
+
+			bool foundExit = false;
+			for (Square* ex : curSq->exits)
+			{
+				if (ex != prevSq)
+				{
+					prevSq = curSq;
+					curSq = ex;
+					foundExit = true;
+					break;
+				}
+			}
+			assert(foundExit, "failed to find exit");
+		}
+
+	}
+
+	void propogateEnclosures()
+	{
+		for (int i = 0; i < 100; i++) //terribly inefficient...
+		{
+			for (const vector<Square>& row : data)
+			{
+				for (const Square& item : row)
+				{
+					if (item.encl == EnclosureStatus::LEFT || item.encl == EnclosureStatus::RIGHT)
+					{
+						if (withinGrid(item.loc.N()) && getSquare(item.loc.N())->encl == EnclosureStatus::UNDEF)
+						{
+							getSquare(item.loc.N())->encl = item.encl;
+						}
+						if (withinGrid(item.loc.S()) && getSquare(item.loc.S())->encl == EnclosureStatus::UNDEF)
+						{
+							getSquare(item.loc.S())->encl = item.encl;
+						}
+						if (withinGrid(item.loc.E()) && getSquare(item.loc.E())->encl == EnclosureStatus::UNDEF)
+						{
+							getSquare(item.loc.E())->encl = item.encl;
+						}
+						if (withinGrid(item.loc.W()) && getSquare(item.loc.W())->encl == EnclosureStatus::UNDEF)
+						{
+							getSquare(item.loc.W())->encl = item.encl;
+						}
+					}
+				}
+			}
+		}
+		
+	}
+
+	int numEnclosedSquares()
+	{
+		int ret = 0;
+		for (const vector<Square>& row : data)
+		{
+			for (const Square& item : row)
+			{
+				if (item.encl == EnclosureStatus::RIGHT)
+					ret++;
 			}
 		}
 		return ret;
@@ -221,8 +408,11 @@ int main()
 	grid.parseData(lines);
 	grid.hookUpS();
 	grid.calcDists();
-	grid.printDists();
+	//grid.printDists();
+	grid.traverseLoopSetEnclosures();
+	grid.propogateEnclosures();
+	grid.printEnclosureStatus();
 
 	cout << "Max dist = " << grid.maxDist() << endl;
-
+	cout << "enclosed sq = " << grid.numEnclosedSquares() << endl;
 }
